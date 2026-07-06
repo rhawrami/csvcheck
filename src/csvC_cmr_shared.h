5ec4362038075e9d3b2672feddcb6a29337c4cd1
r h <ravanhawrami@gmail.com>
2026-07-06 17:03:07 -0500

    simplify prefixxor logic

--- src/csvC_cmr_shared.h
+++ src/csvC_cmr_shared.h
@@ -12,18 +12,12 @@ const uint8_t carriagereturn_char = '\r';
 
 // prefix_xor computes the prefix xor of a 64-bit bitmask.
 inline uint64_t prefix_xor(uint64_t x) {
-    uint64_t scratch = x << 1;
-    x ^= scratch;
-    scratch = x << 2;
-    x ^= scratch;
-    scratch = x << 4;
-    x ^= scratch;
-    scratch = x << 8;
-    x ^= scratch;
-    scratch = x << 16;
-    x ^= scratch;
-    scratch = x << 32;
-    x ^= scratch;
+    x ^= (x << 1);
+    x ^= (x << 2);
+    x ^= (x << 4);
+    x ^= (x << 8);
+    x ^= (x << 16);
+    x ^= (x << 32);
 
     return x;
 }
