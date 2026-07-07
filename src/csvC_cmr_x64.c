64c09186285bc7e8ca0706a19e2b47bc5b4164fd
r h <ravanhawrami@gmail.com>
2026-07-07 16:32:02 -0500

    impl avx2, fix sse2 res overwrite issue

--- src/csvC_cmr_x64.c
+++ src/csvC_cmr_x64.c
@@ -1,7 +1,6 @@
 #include <stdint.h>
 #include <x86intrin.h>
 
-
 #include "csvC_job.h"
 #include "csvC_cmr_shared.h"
 
@@ -44,14 +43,14 @@ _Bool compare_mask_reduce_x64_SSE2(FILE *f_out, parse_job *pj) {
         res_3 = _mm_cmpeq_epi8(grp_1, v_linefeed);
         res_0 = _mm_or_si128(res_0, res_1);
         res_1 = _mm_or_si128(res_2, res_3);
+        m_linefeed_and_delim |= (uint64_t)_mm_movemask_epi8(res_0);
+        m_linefeed_and_delim |= ((uint64_t)_mm_movemask_epi8(res_1) << 16);
         res_0 = _mm_cmpeq_epi8(grp_2, v_delim);
         res_1 = _mm_cmpeq_epi8(grp_3, v_delim);
         res_2 = _mm_cmpeq_epi8(grp_2, v_linefeed);
         res_3 = _mm_cmpeq_epi8(grp_3, v_linefeed);
         res_2 = _mm_or_si128(res_0, res_1);
         res_3 = _mm_or_si128(res_2, res_3);
-        m_linefeed_and_delim |= (uint64_t)_mm_movemask_epi8(res_0);
-        m_linefeed_and_delim |= ((uint64_t)_mm_movemask_epi8(res_1) << 16);
         m_linefeed_and_delim |= ((uint64_t)_mm_movemask_epi8(res_2) << 32);
         m_linefeed_and_delim |= ((uint64_t)_mm_movemask_epi8(res_3) << 48);
 
@@ -112,10 +111,88 @@ _Bool compare_mask_reduce_x64_SSE2(FILE *f_out, parse_job *pj) {
     return true;
 }
 
-void compare_mask_reduce_x64_AVX2(FILE *f_out, parse_job *pj) {
+_Bool compare_mask_reduce_x64_AVX2(FILE *f_out, parse_job *pj) {
+    // comparison vectors
+    __m256i v_delim = _mm256_set1_epi8((char)pj->pj_delim_char);
+    __m256i v_linefeed = _mm256_set1_epi8((char)linefeed_char);
+    __m256i v_quote = _mm256_set1_epi8((char)quote_char);
 
-}
+    __m256i grp_0;
+    __m256i grp_1;
+    __m256i res_0;
+    __m256i res_1;
+    __m256i res_2;
+    __m256i res_3;
 
-void compare_mask_reduce_x64_AVX512(FILE *f_out, parse_job *pj) {
-    
+    uint64_t m_linefeed_and_delim = 0;
+    uint64_t m_quote = 0;
+
+    uint64_t in_quote = (uint64_t)pj->pj_in_quote;
+
+    for (size_t i = 0; i < pj->pj_buf_len; i += 64) {
+        grp_0 = _mm256_loadu_epi8(pj->pj_buf + i);
+        grp_1 = _mm256_loadu_epi8(pj->pj_buf + i + 32);
+
+        // compare_mask_reduce DELIMITER and LINEFEED        
+        res_0 = _mm256_cmpeq_epi8(grp_0, v_delim);
+        res_1 = _mm256_cmpeq_epi8(grp_0, v_linefeed);
+        res_2 = _mm256_cmpeq_epi8(grp_1, v_delim);
+        res_3 = _mm256_cmpeq_epi8(grp_1, v_linefeed);
+        res_0 = _mm256_or_si256(res_0, res_1);
+        res_2 = _mm256_or_si256(res_2, res_3);
+
+        m_linefeed_and_delim |= (uint64_t)_mm256_movemask_epi8(res_0);
+        m_linefeed_and_delim |= ((uint64_t)_mm256_movemask_epi8(res_2) << 32);
+
+        // compare_mask_reduce QUOTE
+        res_0 = _mm256_cmpeq_epi8(grp_0, v_quote);
+        res_1 = _mm256_cmpeq_epi8(grp_1, v_quote);
+        m_quote |= (uint64_t)_mm256_movemask_epi8(res_0);
+        m_quote |= ((uint64_t)_mm256_movemask_epi8(res_1) << 32);
+
+        m_quote |= in_quote;
+        m_quote = prefix_xor(m_quote);
+        in_quote >>= 63;
+
+        m_linefeed_and_delim &= (~m_quote);
+
+        while (m_linefeed_and_delim > 0) {
+            pj->pj_on_field += 1;
+            uint64_t pos = _tzcnt_u64(m_linefeed_and_delim);
+            uint8_t found_char = pj->pj_buf[(size_t)pos+i];
+
+            if (found_char == linefeed_char) {
+                if (pj->pj_on_field != pj->pj_n_fields) {
+                    if (!pj_report_issue(f_out, pj, pji_FIELDCNT)) {
+                        return false;
+                    }
+                }
+
+                if (
+                    ((size_t)pos+i > 0) &&
+                    (   
+                        // missing CR, and need CR
+                        ((pj->pj_buf[(size_t)pos+i-1] ^ carriagereturn_char) && (uint8_t)(pj->pj_use_cr)) ||
+                        // have CR, but don't need CR
+                        !((pj->pj_buf[(size_t)pos+i-1] ^ carriagereturn_char) && !(uint8_t)(pj->pj_use_cr))
+                    )
+                ) {
+                    // places correct issue type for both scenarios
+                    if (!pj_report_issue(f_out, pj, pji_USEDCR - (pj_issue)pj->pj_use_cr)) {
+                        return false;
+                    }
+                }
+
+                pj->pj_on_row += 1;
+                pj->pj_on_field = 0;
+            } 
+            
+            m_linefeed_and_delim &= (UINT64_MAX << pos); 
+        }
+
+    }
+
+    pj->pj_in_quote = in_quote;
+
+    return true;
 }
