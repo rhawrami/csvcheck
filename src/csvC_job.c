c26e5432d6bf6007a17fe1727d8f7dffda8b0b05
r h <ravanhawrami@gmail.com>
2026-07-13 15:18:25 -0500

    fix fread len param, and simplify issue reporting

--- src/csvC_job.c
+++ src/csvC_job.c
@@ -5,15 +5,15 @@
 
 _Bool pj_fread(FILE *f, parse_job *pj) {
     // rm after testing
-    assert(pj->pj_buf_cap / 64 == 0);
+    assert(pj->pj_buf_cap & 63 == 0);
 
     size_t bytes_read = fread(pj->pj_buf, 1, pj->pj_buf_cap, f);
     if (bytes_read < pj->pj_buf_cap) {
         memset(
-            pj->pj_buf + pj->pj_buf_len, 
+            pj->pj_buf + bytes_read, 
             0, 
             // clear bytes in order to safely pass to fns parsing 64B chunks
-            (pj->pj_buf_len + 63) / 64 * 64
+            (bytes_read + 63) / 64 * 64
         ); 
     }
     pj->pj_buf_len = bytes_read;
@@ -36,42 +36,33 @@ void pj_new_job(const char *csv_name, parse_job *pj) {
 }
 
 _Bool pj_report_issue(FILE *out, parse_job *pj, pj_issue issue) {
-    int ret_val;
+    const char *fmt_msg;
+
     switch (issue) {
         case pji_FIELDCNT:
-            ret_val = fprintf(
-                out, 
-                "%s: Row %llu: field count != %llu\n", 
-                pj->pj_name, 
-                pj->pj_on_row, 
-                pj->pj_n_fields
-            );
+            fmt_msg = "%s: Row %llu: field count != %llu\n";
             break;
         case pji_NEEDEDCR:
-            ret_val = fprintf(
-                out,
-                "%s: Row %llu: missing carriage-return (\\r)",
-                pj->pj_name, 
-                pj->pj_on_row 
-            );
+            fmt_msg = "%s: Row %llu: missing carriage-return (\\r)\n";
             break;
         case pji_USEDCR:
-            ret_val = fprintf(
-                out,
-                "%s: Row %llu: used carriage-return (\\r)",
-                pj->pj_name, 
-                pj->pj_on_row 
-            );
+            fmt_msg = "%s: Row %llu: used carriage-return (\\r)\n";
             break;
         case pji_ESCQUOTE:
-            ret_val = fprintf(
-                out,
-                "%s: Row %llu: unescaped quote",
-                pj->pj_name, 
-                pj->pj_on_row 
-            );
+            fmt_msg = "%s: Row %llu: unescaped quote\n";
             break;
+        default:
+            fmt_msg = "DEFAULT\n";
     }
+
+    int ret_val = fprintf(
+        out,
+        fmt_msg,
+        pj->pj_name, 
+        pj->pj_on_row, 
+        pj->pj_n_fields
+    );
+
 
     return (_Bool)(ret_val >= 0);
 }
